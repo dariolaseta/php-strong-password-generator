0ceda55d47a19081cac5a28766f7f295469ffbab
Oxidra <dantelezzo@gmail.com>
2023-07-04 14:53:08 +0200

    init

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Strong password generator</title>
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    </head>
+    <body>
+        
+    </body>
+</html>
